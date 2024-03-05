2116ae7b5cd900f239e61f2f0729943e3d0c45f1
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-06 00:27:17 +0530

    Started the code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
+        int num = 3;
+        int num1 = 24;
+        System.out.println("sum : "+num+num1);
         System.out.println("Hello world");
     }
 }
