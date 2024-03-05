327378f9f3fc238212c5001965514d554a92bba3
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-06 00:31:21 +0530

    Updated the first file

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         int num = 3;
         int num1 = 24;
-        System.out.println("sum : "+num+num1);
+        int result = num+num1;
+        System.out.println("sum : "+result);
         System.out.println("Hello world");
     }
 }
