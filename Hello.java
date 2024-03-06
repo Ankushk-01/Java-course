a4b3e151f75c33e9f6bd6a2c495617f51e640b0b
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-06 23:21:14 +0530

    Updated the code

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello {
         int num = 3;
         int num1 = 24;
         int result = num+num1;
+        float decimal = 45.676f;
+        System.out.println(decimal);
+        
         System.out.println("sum : "+result);
         System.out.println("Hello world");
     }
