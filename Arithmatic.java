7598acac6469fcb4c70b48831462d14db58a8195
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-08 00:18:38 +0530

    Added new file named Arithmatic

---
+++ Arithmatic.java
@@ -0,0 +1,15 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        double x = 6.5;
+        double y = 5.5;
+        System.out.println("x : "+x);
+        System.out.println("y : "+y);
+        System.out.println("x == y : "+( x == y));
+        System.out.println("x<y : "+(x<y));
+        System.out.println("x>y : "+(x>y));
+        System.out.println("x!=y : "+(x!=y));
+        System.out.println("x<=y : "+(x<=y));
+        System.out.println("!(x<=y) : "+(!(x<=y)));
+        System.out.println("x>=y : "+(x>=y));
+    }
+}
