474e853c35dd2548add58a74b479b7317ed0befd
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-08 00:24:23 +0530

    Added new file named Logical Operators

--- Logical.java
+++ Logical.java
@@ -5,7 +5,13 @@ public class Logical {
         double x = 6.5;
         double y = 5.5;
 
-        Boolean result = a <b && x >y;
+        Boolean result = a <b && x >y; // AND Operator 
+        System.out.println("result : "+result);
+
+        result = a>b || x >y; // OR Operator
+        System.out.println("reslut : "+result);
+
+        result = !(a>b || x >y);
         System.out.println("result : "+result);
     }
 }
