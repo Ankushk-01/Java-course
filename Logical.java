6f843a17c968895083ad6a6c3a6208a655a58788
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-08 00:21:49 +0530

    Added new file named Arithmatic

---
+++ Logical.java
@@ -0,0 +1,11 @@
+public class Logical {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        double x = 6.5;
+        double y = 5.5;
+
+        Boolean result = a <b && x >y;
+        System.out.println("result : "+result);
+    }
+}
