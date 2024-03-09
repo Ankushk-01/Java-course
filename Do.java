1e302836bcc1b369cff28a2efad700604593b60b
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-10 00:14:32 +0530

    Do While loop

---
+++ Do.java
@@ -0,0 +1,11 @@
+public class Do {
+    public static void main(String[] args) {
+        int i =0;
+        int result = 0;
+        do {
+            result+=i;
+            i++;
+        } while (i<=10);
+        System.out.println("Result : "+result);
+    }
+}
