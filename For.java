1174c659aaa7118dadac9af35bcb2074e446c755
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-10 00:29:03 +0530

    For loop

---
+++ For.java
@@ -0,0 +1,10 @@
+public class For {
+    public static void main(String[] args) {
+        for(int i =0;i<5;i++){
+            System.out.println("DAY : "+i);
+            for(int j=1;j<9;j++){
+                System.out.println("Hours : "+(j+8)+" TO "+(j+9));
+            }
+        }
+    }
+}
