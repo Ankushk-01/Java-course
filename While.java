e7a5f3bb9b0ca6f48be10d13be774f610243b394
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-10 00:04:29 +0530

    While loop

--- While.java
+++ While.java
@@ -1,5 +1,16 @@
 public class While {
     public static void main(String[] args) {
-        
+        // While loop
+        int i = 2;
+        while (i<=10) {
+            System.out.println("Table : "+i);
+            int j = 1;
+            while (j<=10) {
+                System.out.println(i+" X "+j+" = "+i*j);
+                j++;
+            }
+            i++;
+            System.out.println("---------------------------");
+        }
     }
 }
