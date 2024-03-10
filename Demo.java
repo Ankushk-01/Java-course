6e31494e668a04eef022c10d3db6bdf8c99dffe2
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-11 00:00:51 +0530

    Learning Classes

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        Calculator cal = new Calculator();
+        int result = cal.add(num1, num2);
+        System.out.println("result : "+result);
+    }
+}
+
+
+class Calculator 
+{
+    public int add(int n1,int n2)
+    {
+        return n1+n2;
+    }
+}
