8831b406ec902f6a8f5ae747be95e8a5a180037b
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-12 02:16:15 +0530

    Added the new class array

--- Arrays.java
+++ Arrays.java
@@ -4,14 +4,23 @@ public class Arrays {
         // System.out.println("num : "+num[0]);
         // System.out.println("num : "+num[1]);
         // System.out.println("num : "+num[2]);
-        for(int i =0;i<num.length;i++)
+        // for(int i =0;i<num.length;i++)
+        // {
+        //     System.out.println(num[i]);
+        // }
+        // int num1 [] = new int[4];
+        // for(int i =0;i<num.length;i++)
+        // {
+        //     System.out.println(num1[i]);
+        // }
+
+        int num2 [][] = new int[4][4];
+        for(int i =0;i<num2.length;i++)
         {
-            System.out.println(num[i]);
-        }
-        int num1 [] = new int[4];
-        for(int i =0;i<num.length;i++)
-        {
-            System.out.println(num1[i]);
+            for(int j = 0;j<num2[i].length;j++)
+            {
+                System.out.println("Value : "+num2[i][j]);
+            }
         }
 
     }
