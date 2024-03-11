b2529f395d78bf4d7f4ae45572642d8fe94f279d
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-12 02:08:40 +0530

    Added the new class array

--- Arrays.java
+++ Arrays.java
@@ -1,8 +1,12 @@
 public class Arrays {
     public static void main(String[] args) {
         int num [] = {3,5,6};
-        System.out.println("num : "+num[0]);
-        System.out.println("num : "+num[1]);
-        System.out.println("num : "+num[2]);
+        // System.out.println("num : "+num[0]);
+        // System.out.println("num : "+num[1]);
+        // System.out.println("num : "+num[2]);
+        for(int i =0;i<num.length;i++)
+        {
+            System.out.println(num[i]);
+        }
     }
 }
