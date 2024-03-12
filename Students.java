76dee1eb072d3b79a4089cc931aba66241aa81c8
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-13 00:03:46 +0530

    Add the Student class

---
+++ Students.java
@@ -0,0 +1,37 @@
+public class Students {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+        s1.rollNumber = 1;
+        s1.name = "Ankush";
+        s1.marks = 80;
+
+        s2.rollNumber = 2;
+        s2.name = "Chandan";
+        s2.marks = 85;
+
+        s3.rollNumber = 3;
+        s3.name = "Gagan";
+        s3.marks = 90;
+
+        Student students [] = new Student[3];
+        students[0] =s1;
+        students[1] =s2;
+        students[2] =s3;
+
+        for(int i =0;i<students.length;i++)
+        {
+            System.out.println("Name : "+students[i].name);
+            System.out.println("Roll Number : "+students[i].rollNumber);
+            System.out.println("Marks : "+students[i].marks);
+        }
+    }
+}
+
+class Student{
+    int rollNumber;
+    String name;
+    int marks;
+}
