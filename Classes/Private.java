1c7f1fbf416a35440d81fa6f3fab0b2b9c5d42cb
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-15 00:02:26 +0530

    Added the privae class

---
+++ Classes/Private.java
@@ -0,0 +1,32 @@
+package Classes;
+
+public class Private {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.Details();
+    }
+}
+class Pen {
+    private String cost;
+    private String color;
+    static String brand;
+
+    public String getCost(){
+        return cost;
+    }
+
+    public void setCost(String c){
+        cost = c;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String col){
+        color = col;
+    }
+
+    public void Details(){
+        System.out.println("This pen belongs to : "+brand+" the color of the pen is : "+getColor()+" and the cost of the pen is : "+getCost());
+    }
+
+}
