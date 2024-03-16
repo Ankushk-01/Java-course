18ac158494178d02e363abb13febd13bab0a75fe
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-17 03:12:37 +0530

    Added the constructor class

--- Classes/Inheritance.java
+++ Classes/Inheritance.java
@@ -3,10 +3,17 @@ class A {
     public A(){
         System.out.println(" Inside the A constructor");
     }
+    public A(String name){
+        System.out.println("A class peramitarized constructor with value : "+name);
+    }
 }
 class B extends A {
     public B(){
         System.out.println(" Inside the B constructor");
+    }
+
+    public B(String name){
+        System.out.println("B class peramitarized constructor with value : "+name);
     }
 }
 public class Inheritance {
--- Classes/README.md
+++ Classes/README.md
@@ -4,4 +4,5 @@
 
 in every constructor the first line is `super()` it is not visible but it is there when the constructor created or using the by default constructor and it use this method to call the parent class default constructor. which helps th jdk to intalize the parent class in background.
 
+1. IF we have created the paramitarized constuctor it also call the default constructor of parent class by default.
 For more info see this code [Constuctors.java](Constructors.java)
