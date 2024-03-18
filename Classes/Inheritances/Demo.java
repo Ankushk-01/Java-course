4bee001cf0b6d37b1ab2580dbe263cce0f837568
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-18 22:35:48 +0530

    Added the multi level inheritance

--- Classes/Inheritances/Demo.java
+++ Classes/Inheritances/Demo.java
@@ -1,10 +1,12 @@
 package Classes.Inheritances;
 public class Demo {
     public static void main(String[] args) {
-        AdvCal cal = new AdvCal();
+        // AdvCal cal = new AdvCal();
+        VeryAdvCal cal = new VeryAdvCal();
         System.out.println(cal.divide(7, 3));
         System.out.println(cal.multi(5, 7));
         System.out.println(cal.add(19,2));
         System.out.println(cal.minus(19,2));
+        System.out.println(cal.power(19,2));
     }
 }
---
+++ Classes/Inheritances/VeryAdvCal.java
@@ -0,0 +1,7 @@
+package Classes.Inheritances;
+
+public class VeryAdvCal extends AdvCal{
+    public double power(int a,int b){
+        return Math.pow(a, b);
+    }
+}
