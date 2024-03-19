7daffbcbdd61362e2d9a28fb901538b1f3666083
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-19 22:38:23 +0530

    Method ovveriding concept

---
+++ Classes/Demo.java
@@ -0,0 +1,8 @@
+package Classes;
+import Classes.Inheritances.Cal;
+public class Demo extends Cal{
+    public static void main(String[] args) {
+        // Cal c1 = new Cal();
+        System.out.println(data);
+    }
+}
--- Classes/Inheritances/Cal.java
+++ Classes/Inheritances/Cal.java
@@ -1,6 +1,7 @@
 package Classes.Inheritances;
 
 public class Cal {
+    protected static int data = 3;
     public int add(int a, int b){
         return a+b;
     }
