57779951d6cc64157b706f5a89b4c2c8e32be13e
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-20 22:29:38 +0530

    Added the final keyword class

--- Classes/FinalKeyword.java
+++ Classes/FinalKeyword.java
@@ -11,9 +11,11 @@ class FinalKeyword {
         System.out.println("num : "+num2);
         // num2 = 17; shows error that we cant change the value of an constant value
         // System.out.println("num : "+num2);
-
-        A a = new A();
-        a.printA(); // we can use the class but can't inherit it
+        AdvCal cal = new AdvCal();
+        cal.add(4, 8);
+        cal.show();
+        // A a = new A();
+        // a.printA(); // we can use the class but can't inherit it
         
     }
 }
@@ -22,13 +24,19 @@ class Cal {
     public void show(){
         System.out.println("In the show method of class Cal");
     }
-    public void add(int a,int b){
+    final public void add(int a,int b){
         System.out.println(a+b);
     }
 }
 
 class AdvCal extends Cal{
-
+    // final public void add(int a,int b){
+    //     System.out.println(a+b);
+    // }
+    // can't override the final method we can use it but cant update it
+    public void show(){
+        System.out.println("In the show method of class AdvCal");
+    }
 }
 
 final class A{  // no one can inherit it we can use it by creating the object of it
