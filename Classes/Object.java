e6c3028bd055d9ef6c8edd1ad5da8b3c374c97c4
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-20 23:16:07 +0530

    updated the object file

--- Classes/Object.java
+++ Classes/Object.java
@@ -7,13 +7,16 @@ public class Object {
         Laptop l2 = new Laptop();
         l2.model = "Asus";
         System.out.println(l1.equals(l2)); // we can override this method in our base class
+        System.out.println(l1);  // by default it runs the .toString() method of Object class
     }
 }
 
 class Laptop {
     String model;
     int price;
-
+    public String toString(){
+       return ("Overrided toString method");
+    }
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -23,24 +26,6 @@ class Laptop {
         return result;
     }
 
-    // @Override
-    // public boolean equals(java.lang.Object obj) {
-    //     if (this == obj)
-    //         return true;
-    //     if (obj == null)
-    //         return false;
-    //     if (getClass() != obj.getClass())
-    //         return false;
-    //     Laptop other = (Laptop) obj;
-    //     if (model == null) {
-    //         if (other.model != null)
-    //             return false;
-    //     } else if (!model.equals(other.model))
-    //         return false;
-    //     if (price != other.price)
-    //         return false;
-    //     return true;
-    // }
     public Boolean equals(Laptop l1){
         if (this == l1) {
             return true;
