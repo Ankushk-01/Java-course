0ad9f001c56a1d50d6691b4c27e9e506e79e88fb
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-23 14:08:49 +0530

    Added the anormous inner class

--- Classes/AnormousInnerClass.java
+++ Classes/AnormousInnerClass.java
@@ -2,12 +2,12 @@ package Classes;
 public class AnormousInnerClass {
     // anormous inner class
     public static void main(String[] args) {
-        A obj = new A(){
-            public void show(){
+        A obj = new A() {
+            public void show() {
                 System.out.println("in anormous inner class show method");
             }
         };
-        obj.show();
+        obj.show(); // This will print "in anormous inner class show method"
     }
 }
 
