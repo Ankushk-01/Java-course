9b03aa72375840f466169f3d2b0b25f770a6dd91
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-24 16:12:34 +0530

    Interface added

---
+++ Classes/Interface.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class Interface {
+    public static void main(String[] args) {
+        Computer c1 = new Desktop();
+        Computer c2 = new Laptop();
+        Devloper dev1 = new Devloper();
+        dev1.devApp(c1);
+    }
+}
+
+interface Computer {
+    public void code();
+}
+
+class Desktop implements Computer{
+    public void code(){
+        System.out.println("run,Execution,Compilation of code using desktop");
+    }
+}
+class Laptop implements Computer{
+    public void code(){
+        System.out.println("run, Execution,Compilation of code using Laptop");
+    }
+}
+
+class Devloper{
+    public void devApp(Computer com){
+        com.code();
+    }
+}
