208e0e17689ab475bbb564208c3c61d5091b2960
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-25 13:09:33 +0530

    added the interface class

---
+++ Classes/Interfaces.java
@@ -0,0 +1,25 @@
+package Classes;
+public class Interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class B implements A {
+    public void show(){
+        System.out.println("show method");
+    }
+    public void config(){
+        System.out.println("config method");
+    }
+}
+interface A {
+    int num = 13; // static and abstract 
+    void show();
+    void config(); // public and abstract
+}
+
+interface Car {
+    void blowHorn();
+    void drive();
+}
