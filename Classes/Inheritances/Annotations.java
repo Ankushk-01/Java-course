213db03879e8c625044efc4515d839dc06a41c3f
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-27 22:56:55 +0530

    Annotations in java

--- Classes/Inheritances/Annotations.java
+++ Classes/Inheritances/Annotations.java
@@ -2,7 +2,8 @@ package Classes.Inheritances;
 
 public class Annotations {
     public static void main(String[] args) {
-        
+        B b = new B();
+        b.showTheValueOfTheShowMethod();
     }
 }
 
@@ -13,10 +14,13 @@ class A {
 }
 
 class B extends A{
-    public void showTheValuOfTheShowMethod(){
+    @Override
+    public void showTheValueOfTheShowMethod(){
         System.out.println("Inside the B class show method");
     }
 
     // if we do some typo in the method name no error will show but intentionly we are doing method overriding but not happening actualy 
+    //So we can use annotations to tell the compile what we want to do so that the compiler can send the error if something get wrong at compile time 
+
     
 }
