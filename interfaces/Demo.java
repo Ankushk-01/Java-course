fb723183035a82ecb7e488660016a2e77013b8b9
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-28 23:14:44 +0530

    updated  the interfaces class

--- interfaces/Demo.java
+++ interfaces/Demo.java
@@ -12,9 +12,21 @@ public class Demo{
             System.out.println(" inside the show method");
         };
         obj.show();
-    }
+
+        A obj2 = () -> System.out.println("declaring show method");
+        obj2.show();
+
+        // B obj3 = (int value) -> System.out.println("square root : "+(value*value));
+        B obj3 = (value) -> System.out.println("square root : "+(value*value)); //still works
+
+        obj3.squreRoot(12);
+    } 
 }
 
 interface A {
-    public void show();
+    void show();
+}
+
+interface B {
+    void squreRoot(int value);
 }
