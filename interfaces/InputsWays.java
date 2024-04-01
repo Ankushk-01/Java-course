4a703050e174d4b9540f68758265755fb1925480
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-01 22:25:20 +0530

    updated the inputs ways file

--- interfaces/InputsWays.java
+++ interfaces/InputsWays.java
@@ -7,15 +7,14 @@ public class InputsWays {
         System.out.print("Enter the number : ");
         int num = 0;
         try {
-            num = System.in.read(); // it retruns the ASCII value of each value taking from console 
+            num = System.in.read(); // it retruns the ASCII value of each value taking from console  and also read one characher at a time 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // to resolve this issue remove the value 48 from the num 
 
-        num -= 68;
-        System.out.println("the number is : "+num);
+        System.out.println("the number is : "+(num-48));
         
     }
 }
