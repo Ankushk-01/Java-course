01172ea3ddd4f7e6befa0f9a6ee1eeb7287cf38f
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-02 22:56:06 +0530

    Added the thread class

---
+++ Threads/ThreadClass.java
@@ -0,0 +1,37 @@
+package Threads;
+
+public class ThreadClass {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+        obj1.setPriority(Thread.MAX_PRIORITY);  // Set priority
+        obj1.start();
+        obj2.start();
+    }
+}
+
+
+class A extends Thread {
+    public void run(){
+        for(int i =0;i<5;i++){
+            System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class B extends Thread {
+    public void run(){
+        for(int i =0;i<5;i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
