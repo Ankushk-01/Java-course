cd2242e8ee15f768c985cb19c43fe4f63cf0d1a1
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-15 23:06:20 +0530

    Added the class CalcTest

---
+++ junit/app/src/test/java/junit/CalcTest.java
@@ -0,0 +1,13 @@
+package junit;
+
+import static org.junit.Assert.fail;
+
+// import org.junit.Assert;
+import org.junit.Test;
+public class CalcTest {
+    @Test
+    public void testDivideMethod(){
+        System.out.println("Not implemented");
+        fail("Not implemented"); 
+    }
+}
