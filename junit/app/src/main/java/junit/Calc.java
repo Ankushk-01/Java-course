fe56f1da288b106c0160f398032f22ed11e9ce93
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-17 00:00:30 +0530

    updated the CalTest class

--- junit/app/src/main/java/junit/Calc.java
+++ junit/app/src/main/java/junit/Calc.java
@@ -2,6 +2,9 @@ package junit;
 
 public class Calc {
     public int divide(int num,int num2){
-        return num*num2;
+        return num/num2;
+    }
+    public int multiply(int num1,int num2){
+        return num1*num2;
     }
 }
--- junit/app/src/test/java/junit/CalcTest.java
+++ junit/app/src/test/java/junit/CalcTest.java
@@ -1,6 +1,7 @@
 package junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 // import org.junit.Assert;
 import org.junit.Test;
@@ -11,5 +12,10 @@ public class CalcTest {
         int result = c1.divide(10, 5);
         int expected = 2;
         assertEquals(result, expected);
+        System.out.println("divide method success test case pass");
+
+        expected = 20;
+        assertNotEquals(result, expected);
+        System.out.println("divide method fail test case pass");
     }
 }
