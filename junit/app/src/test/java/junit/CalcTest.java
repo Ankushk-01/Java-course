e35a4a2c5fc4030b24caeb0067dca118fddc8455
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-17 00:03:27 +0530

    updated the CalTest class

--- junit/app/src/test/java/junit/CalcTest.java
+++ junit/app/src/test/java/junit/CalcTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.assertNotEquals;
 // import org.junit.Assert;
 import org.junit.Test;
 public class CalcTest {
+    Calc c1 = new Calc();
     @Test
     public void testDivideMethod(){
-        Calc c1 = new Calc();
         int result = c1.divide(10, 5);
         int expected = 2;
         assertEquals(result, expected);
@@ -17,5 +17,12 @@ public class CalcTest {
         expected = 20;
         assertNotEquals(result, expected);
         System.out.println("divide method fail test case pass");
+    }
+
+    @Test
+    public void testMultiplyMethod(){
+        int result = c1.multiply(2, 5);
+        assertEquals(result, 10);
+        System.out.println("Multiply test case pass");
     }
 }
