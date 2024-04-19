dabea0be7fac4bf0a8b0fda6483d06e1cd3f8af0
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-19 21:45:54 +0530

    add the test cases for reverseInt method

---
+++ Junit-Jupiter/app/src/test/java/_JunitProject/UtilityTest.java
@@ -0,0 +1,21 @@
+package _JunitProject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UtilityTest {
+    Utility utility = new Utility();
+    @Test
+    void reverseStringTest(){
+        assertEquals("hannah", utility.reverseString("hannah"));
+        assertEquals("Ankush", utility.reverseString("hsuknA"));
+        assertEquals("1234", utility.reverseString("4321"));
+    }
+
+    @Test
+    void reverseIntegerTest(){
+        assertEquals(4321, utility.reverseInteger(1234));
+        assertEquals(44664321, utility.reverseInteger(12346644));
+    }
+}
