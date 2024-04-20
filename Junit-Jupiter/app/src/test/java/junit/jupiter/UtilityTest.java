9e35e085d1ffdf5451313e7842db851b233385bb
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-20 14:14:26 +0530

    added the error msgs in the test cases

--- Junit-Jupiter/app/src/test/java/junit/jupiter/UtilityTest.java
+++ Junit-Jupiter/app/src/test/java/junit/jupiter/UtilityTest.java
@@ -9,19 +9,19 @@ public class UtilityTest {
     Utility utility = new Utility();
     @Test
     void reverseStringTest_one_word(){
-        assertEquals("hannah", utility.reverseString("hannah"));
-        assertEquals("Ankush", utility.reverseString("hsuknA"));
-        assertEquals("1234", utility.reverseString("4321"));
+        assertEquals("hannah", utility.reverseString("hannah"),"The expected reverse string is not matching the actual reverse String");
+        assertEquals("Ankush", utility.reverseString("hsuknA"),"The expected reverse string is not matching the actual reverse String");
+        assertEquals("1234", utility.reverseString("4321"),"The expected reverse string is not matching the actual reverse String");
     }
 
     @Test
     void reverseIntegerTest(){
-        assertEquals(4321, utility.reverseInteger(1234));
+        assertEquals(4321, utility.reverseInteger(1234),"the reverse int value function is not working fine");
         assertEquals(44664321, utility.reverseInteger(12346644));
     }
 
     @Test
     void reverseString_multiple_words(){
-        assertEquals("tseb si hsuknA", utility.reverseString("Ankush is best"));
+        assertEquals("tseb si hsuknA", utility.reverseString("Ankush is best"),"the multiline reverse string logic is failing");
     }
 }
