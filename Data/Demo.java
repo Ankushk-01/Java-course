f2b63753931351ce0879466edcb4389f6ab3d0ad
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-25 22:46:13 +0530

    updated the linear search code

--- Data/Demo.java
+++ Data/Demo.java
@@ -6,16 +6,20 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("-------------------Linear Search----------------------");
         int arr[] = {2,3,6,4,2,7,8,1,9};
-        try {
-            Scanner sc = new Scanner(System.in);
+        String str = "";
+        try (Scanner sc = new Scanner(System.in);) {
             System.out.print("Enter the element : ");
             int input = sc.nextInt();
             for(int i =0;i<arr.length;i++){
                 if (arr[i] == input) {
-                    System.out.println("index : "+i);
+                    str+= i+" ";
                 }
             }
-            System.out.println("Element not found");
+            if(str.length()==0){
+                System.out.println("Element not found");
+            }else{
+                System.out.println("index : "+str);
+            }
 
         } catch (Exception e) {
             System.out.println("error : "+e.getMessage());
