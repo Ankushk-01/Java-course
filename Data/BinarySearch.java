7550f4323b524882f6e6aef9b8f7a89a6fd35b8f
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-26 22:47:16 +0530

    aded the binary search file

---
+++ Data/BinarySearch.java
@@ -0,0 +1,12 @@
+package Data;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int input[] = {1,2,3,4,5,6,7,8,9};
+        
+    }
+    public int binarySearch(int [] input,int element){
+        int index =0;
+        return index;
+    }
+}
