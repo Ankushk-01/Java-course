16d99fcb5514ee74f1bcbf7bbec3590341f479e3
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-26 23:02:56 +0530

    updated the binary search file

--- Data/BinarySearch.java
+++ Data/BinarySearch.java
@@ -3,10 +3,24 @@ package Data;
 public class BinarySearch {
     public static void main(String[] args) {
         int input[] = {1,2,3,4,5,6,7,8,9};
+        int result = binarySearch(input, 3);
+        System.out.println("index = "+result);
         
     }
-    public int binarySearch(int [] input,int element){
-        int index =0;
+    public static int binarySearch(int [] input,int element){
+        int index = 0;
+        int left = 0;
+        int right = input.length-1;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(element == input[mid]){
+                return mid;
+            }else if (element<mid) {
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
         return index;
     }
 }
