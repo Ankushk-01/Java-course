e1d225462b0cbab7d5e9b842f361488a53c954e1
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-28 23:31:53 +0530

    Updated the selection sort code

--- Data/SelectionSort.java
+++ Data/SelectionSort.java
@@ -8,11 +8,27 @@ public class SelectionSort {
             System.out.print(element + " ");
         }
         System.out.println();
-        int[] sortedArray = selectionSort(input);
+        // int[] sortedArray = selectionSort(input);
+        int[] sortedArray = selectionSortMax(input);
         System.out.println("after sorting");
         for (int element : sortedArray) {
             System.out.print(element + " ");
         }
+    }
+
+    private static int[] selectionSortMax(int[] input) {
+        for (int i = input.length - 1; i >= 0; i--) {
+            int maxIndex = i;
+            for (int j = i-1; j >=0; j--) {
+                if (input[maxIndex] < input[j]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = input[maxIndex];
+            input[maxIndex] = input[i];
+            input[i] = temp;
+        }
+        return input;
     }
 
     private static int[] selectionSort(int[] input) {
