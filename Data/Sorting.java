56ba924eb7530583ecfdb511d3d69f180f6f1775
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-28 22:34:52 +0530

    updated the bubble sort logic

--- Data/Sorting.java
+++ Data/Sorting.java
@@ -3,30 +3,26 @@ package Data;
 public class Sorting {
     public static void main(String[] args) {
         int[] input = { 2, 5, 3, 1, 7, 6, 4, 9 };
+        System.out.println("before sorting");
+        for (int element : input) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
         int[] sortedArray = bubbleSort(input);
+        System.out.println("after sorting");
         for (int element : sortedArray) {
-            System.out.println(element);
+            System.out.print(element + " ");
         }
     }
 
     private static int[] bubbleSort(int[] input) {
         for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input.length - i; j++) {
-                int first = input[j];
-                int second = 0;
-                if (j == input.length - 1) {
-                    second = j;
-                } else {
-
-                    second = input[j + 1];
-                }
-                if (first < second) {
-                    int temp = first;
-                    first = second;
-                    second = temp;
+            for (int j = 0; j < input.length -1; j++) {
+                if(input[j] > input[j+1]){
+                    int temp = input[j];
+                    input[j] = input[j+1];
+                    input[j+1] = temp;
                 }
             }
-        }
-        return input;
-    }
-}
+        }return input;
+}}
