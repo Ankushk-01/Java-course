3eabca7e5962418796e66dd56a49878a1a97354b
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-28 22:37:48 +0530

    added the indentation

--- Data/Sorting.java
+++ Data/Sorting.java
@@ -17,7 +17,7 @@ public class Sorting {
 
     private static int[] bubbleSort(int[] input) {
         for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input.length - 1; j++) {
+            for (int j = 0; j < input.length-i- 1; j++) {
                 if (input[j] > input[j + 1]) {
                     int temp = input[j];
                     input[j] = input[j + 1];
