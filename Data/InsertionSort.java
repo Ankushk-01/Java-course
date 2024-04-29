3863a6ae5c21dabf945c4dc30b43f9c3567c7caf
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-04-29 22:57:35 +0530

    added the new approach of insertion sort

--- Data/InsertionSort.java
+++ Data/InsertionSort.java
@@ -8,11 +8,25 @@ public class InsertionSort {
             System.out.print(element + " ");
         }
         System.out.println();
-        int[] sortedArray = insertionSort(input);
+        // int[] sortedArray = insertionSort(input);
+        int[] sortedArray = insertionSort2(input);
         System.out.println("after sorting");
         for (int element : sortedArray) {
             System.out.print(element + " ");
         }
+    }
+
+    private static int[] insertionSort2(int[] input) {
+        for(int i =1;i<input.length;i++){
+            int j = i-1;
+            int key = input[i];
+            while (j>=0 && input[j]>key) {
+                input[j+1] = input[j];
+                j--;
+            }
+            input[j+1] = key;
+        }
+        return input;
     }
 
     private static int[] insertionSort(int[] input) {
