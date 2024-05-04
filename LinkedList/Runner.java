04b31216949126e73404236aff58e4a1562df9ea
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-04 15:33:21 +0530

    updated the runner.java file

--- LinkedList/Runner.java
+++ LinkedList/Runner.java
@@ -1,12 +1,27 @@
 package LinkedList;
 
+
 public class Runner {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.insert(4);
-        list.show(); 
-    }
+	public static void main(String[] args) {
+		LinkedList  list = new LinkedList();
+		list.insert(2);
+		list.insert(3);
+		list.insert(4);
+//		list.show();
+//		System.out.println("---------------------------------------------------------");
+		list.insertAtStart(22);
+//		list.show();
+		
+		System.out.println("----------------------------------------------------------");
+		
+		list.insertAt(0, 55);
+		list.show();
+		System.out.println("--------------------------------delete --------------------");
+		list.deleteAt(21);
+		list.show();
+		
+		System.out.println("----------------delete all -------------------------------");
+		list.deleteAll();
+		list.show();
+	}
 }
