bb6c910d22e1a5b0c0dd6f766845949a56a14adb
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-05 21:28:55 +0530

    updated the runner file

--- StackFiles/Runner.java
+++ StackFiles/Runner.java
@@ -1,5 +1,27 @@
-package Stack-files;
+package StackFiles;
 
 public class Runner {
-    
+	public static void main(String[] args) {
+		DStack stack = new DStack();
+//		System.out.println(stack.pop());
+		stack.push(5);
+		stack.show();
+		stack.push(2);
+		stack.show();
+		stack.push(6);
+		stack.show();
+		stack.push(9);
+		stack.show();
+		stack.push(1);
+		stack.show();
+		System.out.println("Top value : "+stack.peek());
+		// stack overflow error 
+//		stack.push(3);
+		
+		
+//		stack.pop();
+//		stack.show();
+//		System.out.println(stack.isEmpty());
+//		System.out.println(stack.size());
+	}
 }
