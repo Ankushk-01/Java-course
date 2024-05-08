f79dc8a5ad4660704afb02dcda3b9c300bdb5dab
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-08 23:15:23 +0530

    added the readme file

--- BinaryTreeFiles/BinaryTree.java
+++ BinaryTreeFiles/BinaryTree.java
@@ -3,31 +3,29 @@ package BinaryTreeFiles;
 public class BinaryTree {
 	private Node root;
 	public void insert(int data) {
-		Node newNode = new Node(data);
-		if(root == null) {
-			root = newNode;
-		}else {
-			Node temp = root;
-			while(temp.left!=null && temp.right!=null) {
-				if(data<temp.data) {
-					temp = temp.left;
-				}else {
-					temp = temp.right;
-				}
-			}
-			if(data<temp.data) {
-				temp.left = newNode;
-			}else {
-				temp.right = newNode;
-			}
-			
-		}
+		root = insertRec(root,data);
 	}
 	
-	public void show() {
-		while(root!=null) {
-			System.out.print(root.data+" ");
-			root = root.right;
+	private Node insertRec(Node root2, int data) {
+		if(root2 == null) {
+			root2 = new Node(data);
+		}else if(data < root2.data) {
+			root2.left = insertRec(root2.left, data);
+		}else if(data > root2.data){
+			root2.right = insertRec(root2.right, data);
+		}
+		return root2;
+	}
+
+	public void inorder() {
+		inorderRec(root);
+	}
+
+	private void inorderRec(Node root2) {
+		if(root2!=null) {
+			inorderRec(root2.left);
+			System.out.print(root2.data+" ");
+			inorderRec(root2.right);
 		}
 	}
 }
--- BinaryTreeFiles/Runner.java
+++ BinaryTreeFiles/Runner.java
@@ -3,11 +3,13 @@ package BinaryTreeFiles;
 public class Runner {
 	public static void main(String[] args) {
 		BinaryTree binaryTree = new BinaryTree();
-		binaryTree.insert(1);
+		binaryTree.insert(8);
+		binaryTree.insert(7);
+		binaryTree.insert(12);
+		binaryTree.insert(15);
 		binaryTree.insert(2);
-		binaryTree.insert(3);
-		System.out.println("Done");
+		binaryTree.insert(5);
+		binaryTree.inorder();
 		
-		binaryTree.show();
 	}
 }
