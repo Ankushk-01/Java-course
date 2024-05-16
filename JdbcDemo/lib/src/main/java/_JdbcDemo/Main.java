159cf65d287b453b27267deac7ca4c5d98a9cc72
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-16 22:24:49 +0530

    updated the jdbc file

--- JdbcDemo/lib/src/main/java/_JdbcDemo/Main.java
+++ JdbcDemo/lib/src/main/java/_JdbcDemo/Main.java
@@ -6,20 +6,34 @@ public class Main {
 		String url = "jdbc:mysql://localhost:3306/sakila";
 		String uname = "root";
 		String pass = "Ankush@123";
-		String query = "SELECT `name` FROM language where language_id = 5;";
+//		String query = "SELECT `name` FROM language where language_id = 5;";
+//		String query = "SELECT * FROM language;";
+//		String query = "insert into language values (7,'Hindi','2006-02-15 05:02:19')";
+		String query = "insert into language values (?,?,?)";
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
+		PreparedStatement pstmt = null;
 		
 		try {
 //			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(url, uname, pass);
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(query);
-			while(rs.next()) {
-				String lang = rs.getString("name");
-				System.out.println("name : "+lang);
-			}
+//			stmt = con.createStatement();
+//			int count = stmt.executeUpdate(query);
+//			System.out.println(count+" rows affected");
+//			rs = stmt.executeQuery(query);
+//			while(rs.next()) {
+//				String lang = rs.getInt(1)+" : "+rs.getString(2);
+//				System.out.println("id : "+lang);
+//			}
+			
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, 8);
+			pstmt.setString(2, "punjabi");
+			pstmt.setString(3, "2024-02-15 05:02:19");
+			int count = pstmt.executeUpdate();
+			System.out.println(count+" rows affected");
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
