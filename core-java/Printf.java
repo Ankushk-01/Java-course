a89f99e88bb84414d922b674f711b48344c912a1
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-28 22:26:54 +0530

    added the printf class

---
+++ core-java/Printf.java
@@ -0,0 +1,28 @@
+public class Printf {
+    public static void main(String[] args) {
+        /* printf method :- this method is ued to format,display and control the text which 
+         can be display on the console 
+         it took the two values :
+         1. text which we want to format
+         2. (object,variable or value)
+
+        % [flags] [precision] [conversion character]
+
+        */
+
+        System.out.printf("This is the sentence %d",123);
+        // [conversion character]
+        int num = 10;
+        boolean bool = true;
+        char c = 'c';
+        String name = "Ankush";
+        double pie = 3.1478d;
+        System.out.printf("the boolean value is :  %b",bool);
+        System.out.printf("the char value is :  %c",c);
+        System.out.printf("the int value is :  %d",num);
+        System.out.printf("the String value is :  %s",name);
+        System.out.printf("the float value is :  %f",pie);
+        
+
+    }
+}
