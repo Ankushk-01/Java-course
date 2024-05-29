b0d3925ccf6b07797ea926e9982b72122afe2235
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-29 22:10:57 +0530

    spelling check

--- core-java/Casting.java
+++ core-java/Casting.java
@@ -11,7 +11,7 @@ public class Casting {
 
         byte num1 = 30;
         byte num2 = 10;
-        int result = num1*num2; // the value of it will be 300 which is higher then the range of byte so it will be permoted to the int data type
+        int result = num1*num2; // the value of it will be 300 which is higher then the range of byte so it can be converted to the int data type
         System.out.println("result : "+result);
         byte trim = (byte) result;
         System.out.println("trim : "+trim);
