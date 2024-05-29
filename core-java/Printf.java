0b5fd6f407b8de2cea80ccdf4390949be5b3d6cb
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-29 22:08:52 +0530

    fasle commit

--- core-java/Printf.java
+++ core-java/Printf.java
@@ -25,8 +25,6 @@ public class Printf {
         System.out.println();
         // %10s makes the minimum string length to 10 and it will add whitespace if the string is short 
         System.out.printf("hello %10s",name);
-
-
         System.out.println();
         System.out.printf("hello %-10s",name); // left justify the spaces
 
