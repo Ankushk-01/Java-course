b2eb7343094c8a6930e4d8af6ec661ca53e06592
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-05-30 21:22:59 +0530

    fasle commit

--- core-java/Casting.java
+++ core-java/Casting.java
@@ -1,6 +1,6 @@
 public class Casting {
     public static void main(String[] args) {
-        //When we do explicit type convertion it is called Casting
+        //When we do explicit type conversion it is called Casting
         int count = 357;
         int data = count%256;
         System.out.println("data : "+data);
