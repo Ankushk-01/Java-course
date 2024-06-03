b3bd01a35618a041f536ad91b4b5da3a82686c34
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-03 22:18:27 +0530

    updated the file reader class

--- Files/ReadFiles.java
+++ Files/ReadFiles.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 
 public class ReadFiles {
     public static void main(String[] args) {
-        try {
-            FileReader fileReader = new FileReader("Text.txt");
+        try (FileReader fileReader = new FileReader("Text.txt");){
             int result = fileReader.read();
             while (result != -1) {
                 System.out.print((char)result);
