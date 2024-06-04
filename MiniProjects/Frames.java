c61cfddaeaa06e86e93a5050be86d0f3eb4af0a2
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-04 22:59:57 +0530

    updated the frames class

--- MiniProjects/Frames.java
+++ MiniProjects/Frames.java
@@ -1,12 +1,20 @@
 package MiniProjects;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
+import java.awt.Color;
+import java.awt.Image;
 
 public class Frames {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(420,420);
-        jFrame.setVisible(true);
+        jFrame.setTitle("Demo JFrame");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close when click on cross button
+        ImageIcon image = new ImageIcon("logo.jpg");
+        jFrame.setIconImage(image.getImage());
+        jFrame.setBackground(Color.GRAY);
+        jFrame.setSize(420,420); // Set Dimensions 
+        jFrame.setVisible(true); // It will visible the frame
     }
 }
