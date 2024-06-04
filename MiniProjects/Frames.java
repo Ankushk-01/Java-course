c9d84292d861e0f61dd6a3bed336dd3e888de212
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-04 22:50:19 +0530

    added the frames class

--- MiniProjects/Frames.java
+++ MiniProjects/Frames.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 public class Frames {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(420,420);
         jFrame.setVisible(true);
     }
 }
