f28ac0fd2ec20f49a1c926786598b569174bfb9e
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-06 23:42:41 +0530

    added the comments

--- MiniProjects/JLabelClass.java
+++ MiniProjects/JLabelClass.java
@@ -10,17 +10,16 @@ import javax.swing.JLabel;
 public class JLabelClass {
     public static void main(String[] args) {
         JLabel jLabel = new JLabel(); // Set label and label is a component
-        jLabel.setText("Welcome to my Channel");
-        ImageIcon imageIcon = new ImageIcon("udemy-logo.png");
-        jLabel.setIcon(imageIcon);  
-        jLabel.setDisabledIcon(imageIcon);
-        jLabel.setIconTextGap(40);
-        jLabel.setHorizontalAlignment(JLabel.CENTER);
-        jLabel.setVerticalAlignment(JLabel.TOP); 
-        jLabel.setFont(new Font("Mv Boli",Font.BOLD,50));
-        jLabel.setForeground(Color.green);
-        jLabel.setBackground(Color.black);
-        jLabel.setOpaque(true);
+        jLabel.setText("Welcome to my Channel"); // Set the text 
+        ImageIcon imageIcon = new ImageIcon("udemy-logo.png"); // Create a image icon object
+        jLabel.setIcon(imageIcon);  // Sets the icon only works with the .png as per my knowledge
+        jLabel.setIconTextGap(40); // Sets the icon and text gap 
+        jLabel.setHorizontalAlignment(JLabel.CENTER); // Set the y axis alignment of content of label
+        jLabel.setVerticalAlignment(JLabel.CENTER);  // Set the x axis alignment of content of label
+        jLabel.setFont(new Font("Mv Boli",Font.BOLD,50)); // Sets the font of the text
+        jLabel.setForeground(Color.green); // color of the font
+        jLabel.setBackground(Color.black); // sets the background color it works only with below method
+        jLabel.setOpaque(true); // make the background of the label is visible 
         
         JFrame jFrame = new JFrame(); // create a JFrame
         jFrame.setTitle("Welcome page");
