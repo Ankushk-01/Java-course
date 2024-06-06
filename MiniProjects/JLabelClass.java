4f2e140006618b39a85d6b03b80d55e1ff731973
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-06 23:12:51 +0530

    updated the Jlabel file

--- MiniProjects/JLabelClass.java
+++ MiniProjects/JLabelClass.java
@@ -8,10 +8,11 @@ public class JLabelClass {
     public static void main(String[] args) {
         JLabel jLabel = new JLabel(); // Set label and label is a component
         jLabel.setText("Welcome to my Channel");
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        jLabel.setVerticalAlignment(JLabel.TOP); 
         ImageIcon imageIcon = new ImageIcon("logo.jpg");
-        jLabel.setIcon(imageIcon);
-
-
+        jLabel;
+        
         JFrame jFrame = new JFrame(); // create a JFrame
         jFrame.setTitle("Welcome page");
         jFrame.setSize(500,500);
