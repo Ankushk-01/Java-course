d111491be750be01b1a7cea1127869d08e21233e
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-07 23:48:32 +0530

    added the JButton project

--- MiniProjects/JButton/Main.java
+++ MiniProjects/JButton/Main.java
@@ -1,20 +1,29 @@
 package MiniProjects.JButton;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        MyFrame frame = new MyFrame();
+        new MyFrame();
     }
 }
 
 
 class MyFrame extends JFrame{
+    JButton button;
     MyFrame(){
-       JFrame jFrame = new JFrame();
-        jFrame.setTitle("Demo JFrame");
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close when click on cross button
-        jFrame.setSize(500,500); // Set Dimensions  
-        jFrame.setVisible(true); // It will visible the frame 
+        button = new JButton("Click here to poo");
+        button.addActionListener(e -> System.out.println("Poo"));
+        button.setBounds(100,100,200,100); 
+        button.setForeground(Color.blue);
+        this.setTitle("Demo JFrame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close when click on cross button
+        this.setSize(500,500); // Set Dimensions  
+        this.setVisible(true); // It will visible the frame 
+        this.getLayeredPane().setBackground(Color.white);
+        this.add(button);
     }
 }
