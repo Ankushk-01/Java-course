8113c8590fe22059234eeb2b6daccac86a8354ed
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-07 23:20:32 +0530

    updated the jPanel class

--- MiniProjects/JPanelClass.java
+++ MiniProjects/JPanelClass.java
@@ -11,6 +11,14 @@ public class JPanelClass {
         redPanel.setBounds(0,0,250,250); 
         redPanel.setBackground(Color.red);
 
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBounds(250,0,250,250); 
+        bluePanel.setBackground(Color.blue);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBounds(0,250,500,250); 
+        greenPanel.setBackground(Color.green);
+
         JFrame jFrame = new JFrame(); // create a JFrame
         jFrame.setTitle("Welcome page");
         jFrame.setSize(500, 500);
@@ -18,5 +26,7 @@ public class JPanelClass {
         jFrame.setVisible(true);
         jFrame.setLayout(null);
         jFrame.add(redPanel);
+        jFrame.add(bluePanel);
+        jFrame.add(greenPanel);
     }
 }
