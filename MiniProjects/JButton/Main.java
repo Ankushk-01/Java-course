8cb8eca7d67c6f27d85950868cedcc5ef5f403ee
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-08 23:15:49 +0530

    Updated the JButton class

--- MiniProjects/JButton/Main.java
+++ MiniProjects/JButton/Main.java
@@ -1,7 +1,12 @@
 package MiniProjects.JButton;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -12,20 +17,36 @@ public class Main {
 }
 
 
-class MyFrame extends JFrame{
+class MyFrame extends JFrame implements ActionListener{
     JButton button;
     MyFrame(){
-        button = new JButton("Click here to poo");
-        button.addActionListener(e -> System.out.println("Poo"));
-        button.setBounds(200,100,100,50); 
+        ImageIcon icon = new ImageIcon("logo.jpg");
+        button = new JButton("Click here");
+        button.addActionListener(this);
+        // button.addActionListener(e -> System.out.println("Poo"));
+        button.setBounds(200,100,150,50); 
         button.setFocusable(false);
-
+        button.setVerticalTextPosition(JButton.BOTTOM); 
+        button.setHorizontalTextPosition(JButton.LEFT);
+        button.setFont(new Font("Comic Sans",Font.BOLD,10));
+        button.setForeground(Color.BLUE);
+        button.setBackground(Color.GRAY);
+        button.setBorder(BorderFactory.createEtchedBorder());
+ 
         this.setTitle("Demo JFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close when click on cross button
         this.setSize(500,500); // Set Dimensions  
         this.setVisible(true); // It will visible the frame 
         // this.getLayeredPane().setBackground(Color.white);
         this.setLayout(null);
+        this.setIconImage(icon.getImage());
         this.add(button); 
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("Poo");
+            button.setEnabled(false);
+        }
     }
 }
