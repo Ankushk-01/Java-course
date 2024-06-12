4a8e5825305f6807180d3b21e992bda26cd85f3b
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-12 22:32:57 +0530

    added the MyFrame class

--- MiniProjects/WindowShift/MyFrame.java
+++ MiniProjects/WindowShift/MyFrame.java
@@ -1,14 +1,30 @@
 package MiniProjects.WindowShift;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends JFrame{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+public class MyFrame extends JFrame implements ActionListener{
     JFrame frame = new JFrame();
-    MyFrame(){
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
-        frame.setLayout(null);
-        frame.setVisible(true); 
-    }
+    MyFrame() {
+        // button
+        JButton button = new JButton("Click me");
+        button.setBounds(200, 200, 100, 50);
+        button.addActionListener(this);
+        button.setFocusable(true);
 
+        // frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.add(button);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Button clicked");
+        OtherWindow window = new OtherWindow();
+        // frame.dispose();
+    }
 }
