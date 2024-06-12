8b7b52a7036b2c03da81e6c34722df6e62a3cddd
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-12 21:20:13 +0530

    added the window class

---
+++ MiniProjects/WindowShift/Main.java
@@ -0,0 +1,7 @@
+package MiniProjects.WindowShift;
+
+public class Main {
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+    }
+}
---
+++ MiniProjects/WindowShift/MyFrame.java
@@ -0,0 +1,14 @@
+package MiniProjects.WindowShift;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+    JFrame frame = new JFrame();
+    MyFrame(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.setVisible(true); 
+    }
+
+}
