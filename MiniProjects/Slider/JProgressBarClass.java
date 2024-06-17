88766b81cca5321dfa4a443342eabb65ca3ea5ac
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-17 22:50:07 +0530

    updated the progress bar class

--- MiniProjects/Slider/JProgressBarClass.java
+++ MiniProjects/Slider/JProgressBarClass.java
@@ -1,5 +1,7 @@
 package MiniProjects.Slider;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,12 +26,28 @@ class MyFrame {
         bar.setValue(0);
         bar.setBounds(0,30,500,70);
         bar.setStringPainted(true); // print the progress status like _% 
+        bar.setForeground(Color.GRAY);
+
         // Frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setLayout(null);
         frame.add(bar);
         frame.setVisible(true);
+        fill();
 
+    }
+    private void fill() {
+        int counter = 0;
+        while(counter <=100){
+            bar.setValue(counter);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } 
+            counter+=10; 
+        }
+        fill();
     }
 } 
