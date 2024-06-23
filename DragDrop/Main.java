992dc9da76f079f0b7d01d2436adab3b1d414798
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-23 22:43:06 +0530

    added the Myframe class

--- DragDrop/Main.java
+++ DragDrop/Main.java
@@ -1,7 +1,26 @@
 package DragDrop;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Main {
     public static void main(String[] args) {
-        
+        MyFrame frame = new MyFrame();
     }
+}
+
+class MyFrame extends JFrame {
+    DragPanel panel = new DragPanel();
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 600);
+        this.setLayout(null);
+        this.setTitle("Mini Project");
+        this.add(panel);
+        this.setVisible(true);
+    }
+}
+
+class DragPanel extends JPanel{
+
 }
