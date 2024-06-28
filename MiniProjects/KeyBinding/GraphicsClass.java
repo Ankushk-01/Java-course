046bc01a0f464a9abbe30c4588ca0ebc19c23f79
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-06-28 12:32:29 +0530

    added the arc and created the different color circle

--- MiniProjects/KeyBinding/GraphicsClass.java
+++ MiniProjects/KeyBinding/GraphicsClass.java
@@ -34,25 +34,31 @@ class MyLabel extends JLabel{
     }
     public void paint(Graphics g){
         Graphics2D g2D = (Graphics2D) g;
-        g2D.setStroke(new BasicStroke(5));
-        g2D.setPaint(Color.RED);
-        g2D.drawLine(0, 0, 500, 500);
+        // g2D.setStroke(new BasicStroke(5));
+        // g2D.setPaint(Color.RED);
+        // g2D.drawLine(0, 0, 500, 500);
 
         // square
 
-        g2D.setPaint(Color.BLUE);
-        g2D.drawRect(10, 10, 100, 100);
+        // g2D.setPaint(Color.BLUE);
+        // g2D.drawRect(10, 10, 100, 100);
 
-        g2D.setPaint(Color.pink);
-        g2D.fillRect(50, 50, 100, 100);
+        // g2D.setPaint(Color.pink);
+        // g2D.fillRect(50, 50, 100, 100);
 
-        g2D.setPaint(Color.GRAY);
-        g2D.drawOval(350, 150, 100, 100);
-        g2D.setPaint(Color.green);
-        g2D.fillOval(350, 50, 100, 100);
+        // g2D.setPaint(Color.GRAY);
+        // g2D.drawOval(350, 150, 100, 100);
+        // g2D.setPaint(Color.green);
+        // g2D.fillOval(350, 50, 100, 100);
 
         // g2D.dispose(); to free the resources 
 
+        g2D.setPaint(Color.RED);
+        g2D.fillArc(50,100,150,150,0,180);
+        g2D.setPaint(Color.white);
+        g2D.fillArc(50,100,150,150,180,180);
+
+        
 
     }
 }
